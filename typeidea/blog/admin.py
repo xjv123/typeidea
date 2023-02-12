6ea569f47d2704666f6cabc435dca78fea548322
xjv123 <xjv1195275315@qq.com>
2023-02-12 21:12:32 +0800

    编辑页面修改样式

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -66,14 +66,34 @@ class PostAdmin(BaseOwnerAdmin):
     actions_on_top = True
     actions_on_bottom = True
 
-    save_on_top = True
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    save_on_top = False
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('collapse',),
+            'fields': ('tag',),
+        }),
     )
+    filter_horizontal = ('tag',)
 
     def operator(self, obj):
         return format_html(
